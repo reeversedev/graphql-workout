1521272dd903e50c74d11e2ccf88b539ce2f11f8
Prateek Gogia <gogia.prateek@hotmail.com>
2018-10-06 22:38:59 +0530

    Added GraphQL-tag

--- Lyrical-GraphQL/client/Components/SongList.js
+++ Lyrical-GraphQL/client/Components/SongList.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
 
 class SongList extends Component {
   render() {
     return <div>Song List</div>;
   }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
 export default SongList;
